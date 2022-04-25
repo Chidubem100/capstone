237184bd0e733af0eff4f0235fcda9a32e5ab73b
Jamie Bower <j_bower@hotmail.co.uk>
2022-04-25 21:21:16 +1000

    chore: add spaces between keywords and curly braces to make code format more readable

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-//Require all the module needed
+// Require all the module needed
 const http = require("http");
 const fs = require("fs");
 const os = require("os")
 
-//Declare a variable that will contain all the os info required
+// Declare a variable that will contain all the os info required
 const osInfo = {
     "uptime": os.uptime(),
     "architecture": os.arch(),
@@ -16,49 +16,49 @@ const osInfo = {
 // console.log(osInfo);
 
 
-//get all the files
+// get all the files
 const homePage = fs.readFileSync('./pages/index.html')
 const aboutPage = fs.readFileSync('./pages/about.html')
 const errorPage = fs.readFileSync('./pages/404.html')
 
-//create a server using the createServer method
-const server = http.createServer((req,res) =>{
+// create a server using the createServer method
+const server = http.createServer((req, res) => {
     const url = req.url;
-    //Home route
-    if (url==='/' && req.method === 'GET'){
-        res.writeHead(200, {'content-type': 'text/html'})
+    // Home route
+    if (url === '/' && req.method === 'GET') {
+        res.writeHead(200, { 'content-type': 'text/html' })
         res.write(homePage)
         res.end();
-        //About route
-    }else if (url === '/about' && req.method === 'GET') {
-        res.writeHead(200, {'content-type': 'text/html'})
+        // About route
+    } else if (url === '/about' && req.method === 'GET') {
+        res.writeHead(200, { 'content-type': 'text/html' })
         res.write(aboutPage)
         res.end();
-        //System route
-    }else if (url === '/sys' && req.method === "GET"){
-        res.writeHead(201, {'content-type': 'text/plain'})
-        
+        // System route
+    } else if (url === '/sys' && req.method === "GET") {
+        res.writeHead(201, { 'content-type': 'text/plain' })
+
         const jsonString = JSON.stringify(osInfo);
         // console.log(jsonString)
-        fs.writeFile('./osinfo.json', jsonString, err =>{
-            if(err){
+        fs.writeFile('./osinfo.json', jsonString, err => {
+            if (err) {
                 console.log(err)
-            }else{
+            } else {
                 console.log('successfully created')
-                res.end(JSON.stringify({message: "Your OS info has been saved successfully"}));
+                res.end(JSON.stringify({ message: "Your OS info has been saved successfully" }));
             }
         });
         //404 route
-    }else{
-        res.writeHead(404, {'content-type': 'text/html'})
+    } else {
+        res.writeHead(404, { 'content-type': 'text/html' })
         res.write(errorPage)
         res.end();
     }
-    
-    
+
+
 });
 
 //create the server port using the http listen method
-server.listen(5000, ()=>{
+server.listen(5000, () => {
     console.log("serever have startd")
 });
